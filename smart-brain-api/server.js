b3a7ca827378684ea34baebf0bfe0b2ab76ba9ba
Luke Stahl <Stahl1109@gmail.com>
2018-03-21 17:29:35 -0500

    bcrypt npm install

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -12,7 +12,6 @@ const database = {
             id: '123',
             name: 'John',
             email: 'john@gmail.com',
-            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -20,10 +19,16 @@ const database = {
             id: '124',
             name: 'Sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
             entries: 0,
             joined: new Date()
         }
+    ],
+    login: [
+      {  
+        id: '987',
+        has: '',
+        email: 'john@gmail.com'
+      }
     ]
 }
 
@@ -32,6 +37,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+    bcrypt.compare("apples", '$2a$10$ZBv/RjrU4u0Eqegu4cHWW.970xNikKJIeVAFYb45POZTMapkwErAq', function(err, res) {
+        console.log('first guess', res)
+    });
+    bcrypt.compare("veggies",'$2a$10$ZBv/RjrU4u0Eqegu4cHWW.970xNikKJIeVAFYb45POZTMapkwErAq', function(err, res) {
+        console.log('second guess', res)
+    });    
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
         res.json('success');
     } else {
@@ -81,6 +92,11 @@ app.post('/image', (req, res) => {
         res.status(400).json('not found');
     }
 })
+
+
+ 
+// // Load hash from your password DB.
+
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000')
