403f0aea5de03147fc16a1878f75f4ba77a3671d
Luke Stahl <Stahl1109@gmail.com>
2018-03-21 19:47:18 -0500

    servers working

--- face/src/App.js
+++ face/src/App.js
@@ -35,9 +35,26 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+loadUser = (data) => {
+this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+    }})
+}
 
 calculateFaceLocation = (data) => {
   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -66,9 +83,26 @@ onButtonSubmit = () => {
   .predict(
     Clarifai.FACE_DETECT_MODEL, 
     this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-    .catch(err => console.log(err));
-}
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, { entries: count}))
+        })
+
+    }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+      .catch(err => console.log(err));
+  }
+
 
 onRouteChange = (route) => {
   if (route === 'signout') {
@@ -91,7 +125,7 @@ onRouteChange = (route) => {
       { route === 'home' 
           ? <div>
                 <Logo />
-                <Rank />
+                <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                 <ImageLinkForm 
                   onInputChange={this.onInputChange} 
                   onButtonSubmit={this.onButtonSubmit}
@@ -100,8 +134,8 @@ onRouteChange = (route) => {
             </div>
             : (
               route === 'signin' 
-              ? <Signin onRouteChange={this.onRouteChange}/>
-              : <Register onRouteChange={this.onRouteChange}/>
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
       }
       </div>
--- face/src/components/Signin/Signin.js
+++ face/src/components/Signin/Signin.js
@@ -18,21 +18,22 @@ class Signin extends React.Component {
 
     onSubmitSignIn = () => {
         fetch('http://localhost:3000/signin', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                email: this.state.signInEmail,
-                password: this.state.signInPassword
-            })
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            email: this.state.signInEmail,
+            password: this.state.signInPassword
+          })
         })
-        .then(response => response.json())
-        .then(data => {
-            if (data === 'success') {
-                this.props.onRouteChange('home')
+          .then(response => response.json())
+          .then(user => {
+            if (user.id) {
+              this.props.loadUser(user)
+              this.props.onRouteChange('home');
             }
-        })  
-    }
-
+          })
+      }
+      
     render() {
         const { onRouteChange } = this.props;
         return (
